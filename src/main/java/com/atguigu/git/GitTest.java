e249b9be285a9e692cb5c56b0108ab84e0ed578a
Sherlock <1361996350@qq.com>
2021-05-29 13:12:31 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author 雷子誉
+ * @date 2021-05-29 13:04
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello git!");
+    }
+}
